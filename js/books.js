e49ee7f7fb4935931c412ff1b4c3d4d45f61a00c
akash-7313 <akashsau79@gmail.com>
2021-09-02 04:56:03 +0600

    second

---
+++ js/books.js
@@ -0,0 +1,88 @@
+const loadbooks = () =>{
+    const input = document.getElementById('input-field');
+    const searchText = input.value;
+    // error handling for empty input
+    if (searchText == '') {
+        alert('plz type a book name')
+    }
+    else {
+        input.value = '';
+        const url = `http://openlibrary.org/search.json?q=${searchText}`;
+        fetch(url)
+            .then(res => res.json())
+            .then(data => displayBooks(data))
+        }
+}
+const displayBooks = data => {
+    // console.log(data);
+    const books = data.docs;
+    console.log(books);
+    // error handling for no results found
+    if(books.length == 0) {
+        alert('no matching result found, plz try again');
+    }
+    else {
+        // total search result
+        const totalResult = document.getElementById('total-result');
+        totalResult.textContent = '';
+        const result = document.createElement('div');
+        result.innerHTML = `
+            <p class="card-text d-inline">Total search resullts found: <span class="fw-bold fs-5">${books.length}</span></p>
+        `
+        totalResult.appendChild(result);
+        // for (const book of books)
+        // displaying search results
+        const parentCall = document.getElementById('display-books');
+        parentCall.textContent = '';
+        
+        books.forEach(book => {
+            console.log(book);
+            const bookImage = ` https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`;
+            const div = document.createElement('div');
+            div.classList.add('col');
+            div.innerHTML = `
+                <div class="card h-100">
+                    <img height="300px" src="${bookImage}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h4 class="card-title">${book.title}</h4>
+                        <h6 class="card-text fst-italic">${book.author_name}</h6>
+                        <p class="card-text">Publisher: ${book.publisher}</p>
+                        <p class="card-text d-inline">First published: <span class="fw-bold fs-5">${book.first_publish_year}</span></p>
+                    </div>
+                </div>
+            `
+            parentCall.appendChild(div);
+        })
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ test.js
@@ -0,0 +1,46 @@
+const array1 = ['akash'];
+for (const arr1 of array1) {
+    // console.log(arr1);
+}
+
+const array2 = [];
+for (const arr2 of array2) {
+    // console.log('nothing',arr2);
+}
+const array3 =['akash','batash','vdgfc','shedhdeh','wderudf'];
+for (const arr3 of array3) {
+    // console.log(arr3);
+}
+console.log(array3);
+const aL = array3.length;
+// console.log(aL);
+
+console.log(akash);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
